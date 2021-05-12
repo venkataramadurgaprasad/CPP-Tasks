f9689db3c5a5062f8a81a25f4312c0e3244f1bca
Kovvuri Venkata Rama Durgaprasad Reddy <69207735+venkataramadurgaprasad@users.noreply.github.com>
2021-05-12 09:48:32 +0530

    Update 4.prime.cpp

--- Problems on numbers/4.prime.cpp
+++ Problems on numbers/4.prime.cpp
@@ -1,6 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Normal approach
+bool is_prime_norm(int n)
+{
+    for(int i=2; i<n; i++)
+        if(n % i == 0)
+            return false;
+    return true;
+}
+
+
+// Optimized approach
+bool is_prime_opt(int n)
+{
+    for(int i=2; i<n/2+1; i++)
+        if(n % i == 0)
+            return false;
+    return true;
+}
+
+// More optimized approach
 bool is_prime(int n)
 {
     for(int i=2; i<sqrt((double)n)+1; i++)
