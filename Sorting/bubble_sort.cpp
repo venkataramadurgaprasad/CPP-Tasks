5876125de2c0b9fcf73eb5e247f20fc6243aacdd
Kovvuri Venkata Rama Durgaprasad Reddy <69207735+venkataramadurgaprasad@users.noreply.github.com>
2021-06-18 08:25:48 +0530

    Create bubble_sort.cpp

---
+++ Sorting/bubble_sort.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+void bubble_sort(int arr[], int len)
+{
+    for(int i=0; i<len-1; i++) // checks from 0 to n-1 elements
+    {
+        int swaps = 0;
+        // it checks until the numbers not upto sorted, for each iteration each element from last will be sorted equal to i
+        for(int j=0; j<len-i-1; j++)
+        {
+            if(arr[j] > arr[j+1]) // check if greater or not
+            {
+                swaps++; // counts swaps
+                swap(arr[j], arr[j+1]); //swaps elements
+            }
+        }
+        if(!swaps) // breaks if no swap done as it will be sorted
+            break;
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++)
+        cin>>arr[i];
+    cout<<"Before Sorted: ";
+    for(int i=0; i<n; i++)
+        cout<<arr[i]<<" ";
+    cout<<endl;
+    bubble_sort(arr, n);
+    cout<<"After Sorted: ";
+    for(int i=0; i<n; i++)
+        cout<<arr[i]<<" ";
+}
